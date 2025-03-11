1e480e68def77bf5bcfa1f96a631ef1b60fffff9
HenricoHosaki <henricohosaki@gmail.com>
2025-03-11 12:50:55 -0300

    Adicionando arquivos

---
+++ src/Classes/Book.java
@@ -0,0 +1,45 @@
+package Classes;
+
+public class Book {
+    private String name;
+    private Integer id;
+    private Boolean isBorrowed;
+
+    public Book(){
+
+    }
+
+    public Book(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(Boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+    @Override
+    public String toString() {
+        return "Book | " + name + " | " + id;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import Classes.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        List<Book> book = new ArrayList<>();
+
+        while(true){
+            System.out.println("INTERFACE");
+            System.out.println("1 - CADASTRAR LIVRO");
+            System.out.println("2 - LISTAR LIVRO");
+            System.out.println("3 - EMPRESTAR LIVRO");
+            System.out.println("4 - DEVOLVER LIVRO");
+            System.out.println("5 - SAIR");
+            int opcao = sc.nextInt();
+            sc.nextLine();
+
+        switch (opcao) {
+            case 1:
+            System.out.println("Cadastrar livro");
+            System.out.println("Enter name");
+            String name = sc.nextLine();
+
+
+            System.out.println("Enter id");
+            int id = sc.nextInt();
+
+            Book books = new Book(name , id);
+            book.add(books);
+                break;
+            case 2:
+                System.out.println("Listar Livros");
+                for(Book livros : book){
+                    System.out.println(livros);
+                }
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                sc.close();
+                return;
+        }
+        }
+
+        }
+
+    }
